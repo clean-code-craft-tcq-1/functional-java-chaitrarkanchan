1eee20404f604e1c8acbe4b7e24230aab6c415ec
chaitrarkanchan <49150199+chaitrarkanchan@users.noreply.github.com>
2021-03-03 00:11:24 +0530

    Update BMS.java

--- vitals/BMS.java
+++ vitals/BMS.java
@@ -2,20 +2,20 @@ package vitals;
 
 public class BMS {
 	static boolean batteryIsOk(float temperature, float soc, float chargeRate) {       
-        StateEstimator stateestimator = new StateEstimator(temperature, soc, chargeRate);
-        return stateestimator.isBatteryOk();
+        StateEstimator state_estimator = new StateEstimator(temperature, soc, chargeRate);
+        return state_estimator.isBatteryOk();
     }
+	
     public static void main(String[] args) {
+    	
+    	StateEstimator state_estimator = new StateEstimator();
         assert(batteryIsOk(25, 70, 0.7f) == true);
-        assert(batteryIsOk(50, 85, 0.0f) == false);
-        assert(batteryIsOk(45, 80, 0.8f) == true);
-        assert(batteryIsOk(50 , 90, 0.9f) == false);
-        assert(batteryIsOk(-1 , 40, 0.6f) == false);
-        assert(batteryIsOk(25 , 15, 0.9f) == false);
-        assert(batteryIsOk(25 , 65, 0.9f) == false);
-        assert(batteryIsOk(50, 65, 0.7f) == false);
-        assert(batteryIsOk(15, 85, 0.7f) == false);
-        assert(batteryIsOk(15, 15, 0.7f) == false);
-        assert(batteryIsOk(15, 25, 0.9f) == false);
+        assert(batteryIsOk(30, 85, 0.0f) == false);
+        assert(batteryIsOk(-1, -20, 0.7f)==false);
+        assert(batteryIsOk(60, 90, 0.9f)==false);
+        assert(state_estimator.isTemperatureWithinRange(25)==true);
+        assert(state_estimator.isTemperatureWithinRange(50)==false);
+        assert(state_estimator.isSocWithinRange(-20)==false);
+        assert(state_estimator.isChargeWithinRange(0.9f)==false);
     }
 }
