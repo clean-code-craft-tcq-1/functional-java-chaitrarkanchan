dfb5604747cfa284f4fc378da9a5475f416a2622
chaitrarkanchan <49150199+chaitrarkanchan@users.noreply.github.com>
2021-03-02 23:22:54 +0530

    Create Constants.java

---
+++ vitals/Constants.java
@@ -0,0 +1,9 @@
+package vitals;
+
+public class Constants {
+	public static float TEMPERATURE_MAX_THRESHOLD = 45;
+	public static float TEMPERATURE_MIN_THRESHOLD = 0;
+	public static float SOC_MAX_THRESHOLD = 80;
+	public static float SOC_MIN_THRESHOLD = 20;
+	public static float CHARGE_RATE_MAX_THRESHOLD = 0.8f;
+}
