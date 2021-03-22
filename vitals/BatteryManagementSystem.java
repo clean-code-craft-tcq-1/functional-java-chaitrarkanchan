5e1b5944e8f24c4f531e2f315aab37f81c9aaa63
chaitrarkanchan <49150199+chaitrarkanchan@users.noreply.github.com>
2021-03-22 07:39:04 +0530

    Update and rename BMS.java to BatteryManagementSystem.java

---
+++ vitals/BatteryManagementSystem.java
@@ -0,0 +1,24 @@
+package vitals;
+
+public class BatteryManagementSystem {
+	static boolean batteryIsOk(float temperature, float soc, float chargeRate) {       
+		CheckAndAlertBatteryFactors factor_estimator = new CheckAndAlertBatteryFactors(temperature, soc, chargeRate);
+       boolean isTempOk=factor_estimator.isTemperatureWithinRange(temperature);
+       boolean isSocOk=factor_estimator.isSocWithinRange(soc);
+       boolean ischargeRateok=factor_estimator.isChargeWithinRange(chargeRate);
+       factor_estimator.printAlertMessage();
+       return isTempOk&&isSocOk&&ischargeRateok;
+    }
+	
+    public static void main(String[] args) {
+    	
+    	CheckAndAlertBatteryFactors factor_estimator = new CheckAndAlertBatteryFactors();
+        assert(batteryIsOk(25, 70, 0.7f) == true);
+        assert(batteryIsOk(30, 85, 0.0f) == false);
+        assert(batteryIsOk(-1, -20, 0.7f)==false);
+        assert(batteryIsOk(60, 90, 0.9f)==false);
+        assert(factor_estimator.isTemperatureWithinRange(25)==true);
+        assert(factor_estimator.isTemperatureWithinRange(50)==false);
+        assert(factor_estimator.isChargeWithinRange(-20)==false);
+    }
+}
